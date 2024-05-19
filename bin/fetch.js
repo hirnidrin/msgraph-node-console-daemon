0cd2c6f1b85ed318d7067f76ef46fe504f342ca7
hirnidrin <info@itsu.ch>
2024-05-19 18:28:40 +0200

    chore: Convert to "standard" style.

--- bin/fetch.js
+++ bin/fetch.js
@@ -1,28 +1,27 @@
-const axios = require('axios');
+const axios = require('axios')
 
 /**
  * Calls the endpoint with authorization bearer token.
  * @param {string} endpoint
  * @param {string} accessToken
  */
-async function callApi(endpoint, accessToken) {
-
-    const options = {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    };
-
-    console.log('Request made to web API at: ' + new Date().toString());
-
-    try {
-        const response = await axios.get(endpoint, options);
-        console.log('Response HTTP status code: ' + response.status)
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        return error;
+async function callApi (endpoint, accessToken) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
     }
+  }
+
+  console.log('Request made to web API at: ' + new Date().toString())
+
+  try {
+    const response = await axios.get(endpoint, options)
+    console.log('Response HTTP status code: ' + response.status)
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return error
+  }
 };
 
 /**
@@ -31,28 +30,27 @@ async function callApi(endpoint, accessToken) {
  * @param {string} accessToken
  * @param {string} data
  */
-async function postApi(endpoint, accessToken, data) {
-
-    const options = {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    };
-
-    console.log('Request made to web API at: ' + new Date().toString());
-
-    try {
-        const response = await axios.post(endpoint, data, options);
-        console.log('Response HTTP status code: ' + response.status)
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        return error;
+async function postApi (endpoint, accessToken, data) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
     }
+  }
+
+  console.log('Request made to web API at: ' + new Date().toString())
+
+  try {
+    const response = await axios.post(endpoint, data, options)
+    console.log('Response HTTP status code: ' + response.status)
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return error
+  }
 };
 
 module.exports = {
-    callApi: callApi,
-    postApi: postApi
-};
+  callApi,
+  postApi
+}
